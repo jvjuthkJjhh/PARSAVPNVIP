4ecc6218cca7e157dd167c71540ff993495c6dd6
jvjuthkJjhh <p20381539@gmail.com>
2026-09-11 15:57:59 +0330

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'theme/app_theme.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+  runApp(const ParsaVpnApp());
+}
+
+class ParsaVpnApp extends StatelessWidget {
+  const ParsaVpnApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Parsa VPN',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      home: const SplashScreen(),
+    );
+  }
+}
