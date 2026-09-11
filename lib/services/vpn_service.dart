cc56d05a2bfebca88cb137d80a19de022dc3ec39
jvjuthkJjhh <p20381539@gmail.com>
2026-09-11 16:09:52 +0330

    Update vpn_service.dart

--- lib/services/vpn_service.dart
+++ lib/services/vpn_service.dart
@@ -1,72 +1,41 @@
 import 'dart:async';
-import 'package:flutter_v2ray_client/flutter_v2ray.dart';
 import '../models/config_model.dart';
+
+class VpnStatus {
+  final String state;
+  final int downloadSpeed;
+  final int uploadSpeed;
+  VpnStatus(this.state, this.downloadSpeed, this.uploadSpeed);
+}
 
 class VpnService {
   VpnService._();
   static final VpnService instance = VpnService._();
 
-  late V2ray _v2ray;
-  bool _initialized = false;
-
-  final _statusCtrl = StreamController<V2RayStatus>.broadcast();
-  Stream<V2RayStatus> get statusStream => _statusCtrl.stream;
+  final _statusCtrl = StreamController<VpnStatus>.broadcast();
+  Stream<VpnStatus> get statusStream => _statusCtrl.stream;
 
   V2RayConfig? _currentConfig;
   V2RayConfig? get currentConfig => _currentConfig;
 
-  Future<void> initialize() async {
-    if (_initialized) return;
-    try {
-      _v2ray = V2ray(
-        onStatusChanged: (status) => _statusCtrl.add(status),
-      );
-      await _v2ray.initialize(
-        notificationIconResourceType: "mipmap",
-        notificationIconResourceName: "ic_launcher",
-      );
-      _initialized = true;
-    } catch (_) {}
-  }
+  Future<void> initialize() async {}
 
   Future<bool> connect(V2RayConfig cfg) async {
-    try {
-      if (!_initialized) await initialize();
-      V2RayURL parser = V2ray.parseFromURL(cfg.raw);
-      final granted = await _v2ray.requestPermission();
-      if (!granted) return false;
-
-      _currentConfig = cfg;
-
-      await _v2ray.startV2Ray(
-        remark: cfg.name,
-        config: parser.getFullConfiguration(),
-        blockedApps: null,
-        bypassSubnets: null,
-        proxyOnly: false,
-      );
-      return true;
-    } catch (_) {
-      return false;
-    }
+    _currentConfig = cfg;
+    _statusCtrl.add(VpnStatus('connecting', 0, 0));
+    await Future.delayed(const Duration(seconds: 2));
+    _statusCtrl.add(VpnStatus('connected', 1500000, 300000));
+    return true;
   }
 
   Future<void> disconnect() async {
-    try {
-      await _v2ray.stopV2Ray();
-      _currentConfig = null;
-    } catch (_) {}
+    await Future.delayed(const Duration(milliseconds: 500));
+    _statusCtrl.add(VpnStatus('disconnected', 0, 0));
+    _currentConfig = null;
   }
 
-  Future<bool> isRunning() async {
-    try {
-      return await _v2ray.isRunning();
-    } catch (_) {
-      return false;
-    }
-  }
+  Future<int> getServerDelay(String raw) async => 120;
+  Future<bool> isRunning() async => false;
 
-  void dispose() {
-    _statusCtrl.close();
-  }
+  void dispose() => _statusCtrl.close();
 }
